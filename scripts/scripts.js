5adea852713f99eb28f5116a007e4e9e8eb138be
Mohammad <108111892+Mohammad7558@users.noreply.github.com>
2025-03-12 18:38:55 +0600

    added-error-section

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,11 +4,21 @@ const loadCategoryButton = () => {
     .then((data) => displayDataButton(data.categories));
 };
 
+const displayCategoryVideos = (id) => {
+  const url = `https://openapi.programming-hero.com/api/phero-tube/category/${id}`;
+  fetch(url)
+  .then((res) => res.json())
+  .then((data) => displayVideos(data.category))
+}
+
 const displayDataButton = (categories) => {
   const categoryContainer = document.getElementById("category-container");
   for (const categoryName of categories) {
     const { category } = categoryName;
     const categoryButton = document.createElement("button");
+    categoryButton.addEventListener('click', function(){
+      displayCategoryVideos(categoryName.category_id)
+    })
     categoryButton.classList.add("btn", "btn-sm");
     categoryButton.innerText = category;
     categoryContainer.appendChild(categoryButton);
@@ -18,18 +28,27 @@ const displayDataButton = (categories) => {
 const loadVideos = () => {
   fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
     .then((res) => res.json())
-    .then((data) => displayVideos(data));
+    .then((data) => displayVideos(data.videos));
 };
 
 const displayVideos = (videosData) => {
-  const videoContainer = document.getElementById("video-container");
-  const videos = videosData.videos;
-  videos.forEach((video) => {
-    const authorProfile = video?.authors[0]?.profile_picture;
-    const authorName = video?.authors[0]?.profile_name;
-    const views = video?.others?.views;
-    const { title, thumbnail } = video;
-    const videoCard = document.createElement("div");
+  const videoContainer = document.getElementById('video-container');
+  videoContainer.innerHTML = '';
+
+  if(videosData.length === 0){
+    videoContainer.innerHTML = `
+    <div class="col-span-full flex flex-col justify-center items-center">
+        <img src="./images/Icon.png" alt="">
+        <h1>Oops!! No Video Available</h1>
+      </div>
+    `
+  }
+
+  videosData.forEach(video => {
+    console.log(video);
+    const {thumbnail, title, authors, others} = video;
+    console.log(others);
+    const videoCard = document.createElement('div');
     videoCard.innerHTML = `
         <div class="card bg-base-100">
             <figure class="">
@@ -40,24 +59,23 @@ const displayVideos = (videosData) => {
             </figure>
                 <div class="mt-5 flex gap-x-5">
                     <div>
-                        <img class="w-10 h-10 p-[2px] object-cover rounded-full border border-gray-400" src="${authorProfile}" alt="">
+                        <img class="w-10 h-10 p-[2px] object-cover rounded-full border border-gray-400" src="${authors[0].profile_picture}" alt="">
                     </div>
                     <div>
                         <h2 class="card-title">${title}</h2>
                         <div class="flex items-center gap-x-3">
-                            <p class="text-gray-600">${authorName}</p>
+                            <p class="text-gray-600">${authors[0].profile_name}</p>
                             <img class="h-5 w-5" src="https://img.icons8.com/?size=100&id=yuCwxPX1Yonx&format=png&color=000000"> 
                         </div>
-                        <p>${views} views</p>
+                        <p>${others.views} views</p>
                     </div>
                 
                 </div>
             </div>
     </div>
         `;
-    videoContainer.appendChild(videoCard);
+        videoContainer.appendChild(videoCard)
   });
 };
 
 loadCategoryButton();
-loadVideos();
